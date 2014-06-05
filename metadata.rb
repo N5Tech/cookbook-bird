b782d048627d38553b8b7b848af94898185c7305
Todd Crane <todd.crane@n5tech.com>
2014-06-05 16:12:26 -0700

    Replaced placeholders with project info
    Signed-off-by: Todd Crane <todd.crane@n5tech.com>

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'bird'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'N5 Tech, LLC.'
+maintainer_email 'opensource+cookbookbird@n5tech.com'
 license          'All rights reserved'
 description      'Installs/Configures bird'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: bird
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, N5 Tech, LLC.
 #
 # All rights reserved - Do Not Redistribute
 #
